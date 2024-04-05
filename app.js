e6e98e4cb43a41ed1c095b3ed72dbbe411f982a1
YuryF39 <malej100403@gmail.com>
2024-04-05 10:59:28 -0500

    segundo commit

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.use(express.json());
 
 app.use("/api/users",userRoutes);
 
-app.listen(process.env.PORT,process.env.IP_ADDRESS,()=>{
-    console.log('Servidor corriendo en http://'+process.env.IP_ADDRESS+':'+process.env.PORT);
+app.listen(process.env.PORT,()=>{
+    console.log('Servidor corriendo en http://:'+process.env.PORT);
 })
 
 //console.log(`Servidor Corriendo en http://${process.env.IP_ADDRESS}:${process.env.PORT}`)
